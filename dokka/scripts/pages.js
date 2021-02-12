ed51904a81942d740c8214a42e18766e9bfe76f4
nlasagni <nicola.lasagni@gmail.com>
2021-02-12 13:46:50 +0000

    Deploying to gh-pages from @ nlasagni/deleteme-fcla@5ab5502856305d6fe68ae9264406e2823d607878 🚀

---
+++ dokka/scripts/pages.js
@@ -0,0 +1,4 @@
+var pages = [{'name': 'class MainClass', 'description':'com.deletemefcla.MainClass', 'location':'deleteme-fcla/com.deletemefcla/-main-class/index.html', 'searchKey':'MainClass'},
+{'name': 'object MainClass', 'description':'com.deletemefcla.MainClass.MainClass', 'location':'deleteme-fcla/com.deletemefcla/-main-class/-main-class/index.html', 'searchKey':'MainClass'},
+{'name': 'fun printHelloWorld()', 'description':'com.deletemefcla.MainClass.MainClass.printHelloWorld', 'location':'deleteme-fcla/com.deletemefcla/-main-class/-main-class/print-hello-world.html', 'searchKey':'printHelloWorld'},
+{'name': 'fun main()', 'description':'com.deletemefcla.main', 'location':'deleteme-fcla/com.deletemefcla/main.html', 'searchKey':'main'}]
