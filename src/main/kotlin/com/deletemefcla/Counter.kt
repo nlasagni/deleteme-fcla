b969b6bb7d23e0299896017dffee96b8ea0da250
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-02-13 00:17:47 +0100

    Add counter class in order to modify coverage

--- src/main/kotlin/com/deletemefcla/Counter.kt
+++ src/main/kotlin/com/deletemefcla/Counter.kt
@@ -4,4 +4,21 @@ package com.deletemefcla
  * @author Nicola Lasagni on 13/02/2021.
  */
 class Counter {
+
+    private var value: Int = 0
+
+    fun inc() {
+        value++
+    }
+
+    fun dec() {
+        if (value > 0) {
+            value--
+        }
+    }
+
+    fun getValue(): Int {
+        return value
+    }
+
 }
--- src/test/kotlin/com/deletemefcla/CounterTest.kt
+++ src/test/kotlin/com/deletemefcla/CounterTest.kt
@@ -1,7 +1,22 @@
 package com.deletemefcla
 
+import io.kotest.core.spec.style.FreeSpec
+
 /**
  * @author Nicola Lasagni on 13/02/2021.
  */
-class CounterTest {
-}
+class CounterTest : FreeSpec({
+    "New counter is zero" - {
+        assert(Counter().getValue() == 0)
+    }
+    "Counter cannot be negative" - {
+        val counter = Counter()
+        counter.dec()
+        assert(Counter().getValue() == 0)
+    }
+    "Counter is incremented by one per time" - {
+        val counter = Counter()
+        counter.inc()
+        assert(Counter().getValue() == 1)
+    }
+})
