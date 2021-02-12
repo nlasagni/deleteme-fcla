91efe87c45074a6425856ffef2ea9488916baab4
nlasagni <nicola.lasagni@gmail.com>
2021-02-12 13:45:26 +0000

    Deploying to gh-pages from @ nlasagni/deleteme-fcla@5ab5502856305d6fe68ae9264406e2823d607878 🚀

--- scripts/pages.js
+++
@@ -1,4 +0,0 @@
-var pages = [{'name': 'class MainClass', 'description':'com.deletemefcla.MainClass', 'location':'deleteme-fcla/com.deletemefcla/-main-class/index.html', 'searchKey':'MainClass'},
-{'name': 'object MainClass', 'description':'com.deletemefcla.MainClass.MainClass', 'location':'deleteme-fcla/com.deletemefcla/-main-class/-main-class/index.html', 'searchKey':'MainClass'},
-{'name': 'fun main()', 'description':'com.deletemefcla.MainClass.MainClass.main', 'location':'deleteme-fcla/com.deletemefcla/-main-class/-main-class/main.html', 'searchKey':'main'},
-{'name': 'fun main()', 'description':'com.deletemefcla.main', 'location':'deleteme-fcla/com.deletemefcla/main.html', 'searchKey':'main'}]
