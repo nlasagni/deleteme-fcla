ce30d80b7c22e86a8b26db3c74c14006d1fda292
nlasagni <nicola.lasagni@gmail.com>
2021-03-11 17:54:12 +0000

    Deploying to gh-pages from @ nlasagni/deleteme-fcla@1e3fd0fb1fa9016eabf90a135e408dae1fc885a0 🚀

--- scripts/pages.js
+++
@@ -1,9 +0,0 @@
-var pages = [{'name': 'class Counter', 'description':'com.deletemefcla.Counter', 'location':'deleteme-fcla/com.deletemefcla/-counter/index.html', 'searchKey':'Counter'},
-{'name': 'fun Counter()', 'description':'com.deletemefcla.Counter.Counter', 'location':'deleteme-fcla/com.deletemefcla/-counter/-counter.html', 'searchKey':'Counter'},
-{'name': 'fun dec()', 'description':'com.deletemefcla.Counter.dec', 'location':'deleteme-fcla/com.deletemefcla/-counter/dec.html', 'searchKey':'dec'},
-{'name': 'fun getValue(): Int', 'description':'com.deletemefcla.Counter.getValue', 'location':'deleteme-fcla/com.deletemefcla/-counter/get-value.html', 'searchKey':'getValue'},
-{'name': 'fun inc()', 'description':'com.deletemefcla.Counter.inc', 'location':'deleteme-fcla/com.deletemefcla/-counter/inc.html', 'searchKey':'inc'},
-{'name': 'class MainClass', 'description':'com.deletemefcla.MainClass', 'location':'deleteme-fcla/com.deletemefcla/-main-class/index.html', 'searchKey':'MainClass'},
-{'name': 'fun MainClass()', 'description':'com.deletemefcla.MainClass.MainClass', 'location':'deleteme-fcla/com.deletemefcla/-main-class/-main-class.html', 'searchKey':'MainClass'},
-{'name': 'fun generateHelloWorld(): String', 'description':'com.deletemefcla.MainClass.MainClass.generateHelloWorld', 'location':'deleteme-fcla/com.deletemefcla/-main-class/-main-class/generate-hello-world.html', 'searchKey':'generateHelloWorld'},
-{'name': 'fun main()', 'description':'com.deletemefcla.main', 'location':'deleteme-fcla/com.deletemefcla/main.html', 'searchKey':'main'}]
