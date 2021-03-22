31ea75e6bbcc9bb3cb23b187fcf92d04bca6f871
nlasagni <nicola.lasagni@gmail.com>
2021-03-22 17:41:08 +0000

    Deploying to gh-pages from @ nlasagni/deleteme-fcla@d29ba2a8ca72397e95bb4a9ef5d609723737add1 🚀

---
+++ app/scripts/pages.js
@@ -0,0 +1,5 @@
+var pages = [{'name': 'class AppCounter', 'description':'com.deletemefcla.app.AppCounter', 'location':'app/com.deletemefcla.app/-app-counter/index.html', 'searchKey':'AppCounter'},
+{'name': 'fun AppCounter()', 'description':'com.deletemefcla.app.AppCounter.AppCounter', 'location':'app/com.deletemefcla.app/-app-counter/-app-counter.html', 'searchKey':'AppCounter'},
+{'name': 'fun dec()', 'description':'com.deletemefcla.app.AppCounter.dec', 'location':'app/com.deletemefcla.app/-app-counter/dec.html', 'searchKey':'dec'},
+{'name': 'fun getValue(): Int', 'description':'com.deletemefcla.app.AppCounter.getValue', 'location':'app/com.deletemefcla.app/-app-counter/get-value.html', 'searchKey':'getValue'},
+{'name': 'fun inc()', 'description':'com.deletemefcla.app.AppCounter.inc', 'location':'app/com.deletemefcla.app/-app-counter/inc.html', 'searchKey':'inc'}]
