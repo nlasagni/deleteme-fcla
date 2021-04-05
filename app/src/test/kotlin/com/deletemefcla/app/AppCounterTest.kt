26dcf93be09ac4cda4509dbc35b3ce89243cd7dd
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-04-05 16:00:00 +0200

    Tries aggregated jacoco reports and removes one test

--- app/src/test/kotlin/com/deletemefcla/app/AppCounterTest.kt
+++ app/src/test/kotlin/com/deletemefcla/app/AppCounterTest.kt
@@ -9,11 +9,11 @@ class AppCounterTest : FreeSpec({
     "New counter is zero" - {
         assert(AppCounter().getValue() == 0)
     }
-    "Counter cannot be negative" - {
-        val counter = AppCounter()
-        counter.dec()
-        assert(AppCounter().getValue() == 0)
-    }
+//    "Counter cannot be negative" - {
+//        val counter = AppCounter()
+//        counter.dec()
+//        assert(AppCounter().getValue() == 0)
+//    }
     "Counter is incremented by one per time" - {
         val counter = AppCounter()
         counter.inc()
